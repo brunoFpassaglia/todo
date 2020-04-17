ee613916eb483ff8e4b1d6d1b3973087c7c6aca2
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-04-16 21:21:57 -0300

    Subject: create main widget Problem: Solution: Note:

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,111 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _toDoList = [];
+  final _toDoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    Map<String, dynamic> newToDo = Map();
+    newToDo["title"] = _toDoController.text;
+    newToDo["ok"] = false;
+    _toDoController.text = "";
+    setState(() {
+      _toDoList.add(newToDo);
+    });
+    _saveData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ToDo List",
+          style: TextStyle(
+            color: Colors.white70,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: InputDecoration(
+                        labelText: "nova tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    _addToDo();
+                  },
+                  color: Colors.blueAccent,
+                  child: Text("Incluir"),
+                  textColor: Colors.white70,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: 10, bottom: 10, left: 15),
+              itemCount: _toDoList.length,
+              itemBuilder: itemBuilder,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget itemBuilder(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"]
+              ? Icons.check_box
+              : Icons.check_box_outline_blank),
+        ),
+        onChanged: (value) {
+          setState(() {
+            _toDoList[index]["ok"] = !_toDoList[index]["ok"];
+            _saveData();
+          });
+        },
+      ),
+      background: Container(
+        child: Icon(
+          Icons.delete,
+          color: Colors.teal,
+        ),
+        color: Colors.red,
+      ),
+      direction: DismissDirection.startToEnd,
+    );
   }
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
+    print("${directory.path}");
     return File("${directory.path}/data.json");
   }
 
