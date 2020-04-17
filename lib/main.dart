9a4df478423d8cfd19110680117fad8d23a2e978
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-04-17 15:11:23 -0300

    Subject: implement sncack bar to handle delete and undo deletion of todos. Problem: Solution: Note:

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
   final _toDoController = TextEditingController();
 
   @override
@@ -110,13 +112,38 @@ class _HomeState extends State<Home> {
         },
       ),
       background: Container(
-        child: Icon(
-          Icons.delete,
-          color: Colors.teal,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Icon(
+            Icons.delete,
+            color: Colors.teal[50],
+          ),
         ),
         color: Colors.red,
       ),
       direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Todo ${_lastRemoved["title"]} removido"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 3),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
